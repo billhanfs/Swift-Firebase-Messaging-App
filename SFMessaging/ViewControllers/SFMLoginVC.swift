8ba2ed65d5f6915e885b31bcf4ff9f44e7c34399
Bill Han <billhanfs@outlook.com>
2016-10-01 09:03:24 +0800

    Remove Comment

--- SFMessaging/ViewControllers/SFMLoginVC.swift
+++ SFMessaging/ViewControllers/SFMLoginVC.swift
@@ -15,7 +15,7 @@ class SFMLoginVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
