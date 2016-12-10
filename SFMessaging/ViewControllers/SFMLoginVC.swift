74503040708e07920f4ecc92ead5f776d23ff23e
Bill Han <billhanfs@outlook.com>
2016-12-10 08:58:37 +0800

    Remove memory warning method

--- SFMessaging/ViewControllers/SFMLoginVC.swift
+++ SFMessaging/ViewControllers/SFMLoginVC.swift
@@ -22,11 +22,6 @@ class SFMLoginVC: UIViewController {
         _emailTF.becomeFirstResponder()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     // MARK: - Button Events -
     
     @IBAction func onSignInBtn_Pressed(_ sender: AnyObject) {
