17e05e91df01306de87e20fa780ae8882b9055c9
Bill Han <billhanfs@outlook.com>
2017-03-04 08:47:45 +0800

    Create SignUp ViewController

---
+++ SFMessaging/ViewControllers/SFMSignUpVC.swift
@@ -0,0 +1,35 @@
+//
+//  SFMSignUpVC.swift
+//  SFMessaging
+//
+//  Created by Bill Han on 4/3/2017.
+//  Copyright © 2017 Han. All rights reserved.
+//
+
+import UIKit
+
+class SFMSignUpVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
